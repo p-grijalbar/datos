0d6059ded77f7d8fbf385fb5d00a00f7c3797d5d
p-grijalbar <p.grijalbar@uniandes.edu.co>
2023-09-17 13:24:51 -0500

    Add files via upload

---
+++ Proveedor.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM WWImporters_DWH_tablero.Proveedor
+-- Date: 2023-09-17 13:22
+*/
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (NULL,'missing',NULL,NULL,NULL,0,0);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (5,'Graphic Design Institute',29,64847,'productos novedosos',14,1);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (9,'Nod Publishers',37,27906,'productos novedosos',7,2);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (12,'The Phone Company',43,56732,'productos novedosos',30,3);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (2,'Contoso, Ltd.',23,98253,'productos novedosos',7,4);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (8,'Lucerne Publishing',35,37659,'productos novedosos',30,5);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (1,'A Datum Corporation',21,46077,'productos novedosos',14,6);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (10,'Northwind Electric Cars',39,7860,'juguetes',30,7);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (4,'Fabrikam, Inc.',27,40351,'ropa',30,8);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (7,'Litware, Inc.',33,95245,'embalaje',30,9);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (3,'Consolidated Messenger',25,94101,'servicios de mensajeria',30,10);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (13,'Woodgrove Bank',45,94101,'servicios financieros',7,11);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (11,'Trey Research',41,57543,'servicios de marketing',7,12);
+INSERT INTO `` (`ID_Proveedor_T`,`Nombre`,`Contacto_Principal`,`Codigo_Postal`,`Categoria`,`Dias_pago`,`ID_Proveedor_DWH`) VALUES (6,'Humongous Insurance',31,37770,'servicios de seguros',14,13);
