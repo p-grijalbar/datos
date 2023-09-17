f542b09f9114f7ac210283f5e5565764b00da0bf
p-grijalbar <p.grijalbar@uniandes.edu.co>
2023-09-17 13:24:25 -0500

    Add files via upload

---
+++ TipoTransaccion.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM WWImporters_DWH_tablero.TipoTransaccion
+-- Date: 2023-09-17 13:23
+*/
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (2,'Customer Credit Note',1);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (3,'Customer Payment Received',2);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (4,'Customer Refund',3);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (5,'Supplier Invoice',4);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (6,'Supplier Credit Note',5);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (7,'Supplier Payment Issued',6);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (8,'Supplier Refund',7);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (9,'Stock Transfer',8);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (10,'Stock Issue',9);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (11,'Stock Receipt',10);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (12,'Stock Adjustment at Stocktake',11);
+INSERT INTO `` (`ID_Tipo_transaccion_T`,`Tipo`,`ID_Tipo_transaccion_DWH`) VALUES (13,'Customer Contra',12);
